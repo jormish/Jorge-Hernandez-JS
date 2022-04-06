7f9cb139c549a7f8bb78577855f8d2a8b1eb63ec
JorgeHernandez <troprain@gmail.com>
2022-04-06 00:08:07 -0500

    Clase 4 Desafio

--- reservacion.js
+++ reservacion.js
@@ -8,3 +8,18 @@ for(let cuarto = 1; cuarto <= cuartos; cuarto++) {
 
 }
 
+
+alert('Calculando el precio de Tacos, Pizza con soda.... ' )
+
+let result = 0 
+function sumar(c1,c2){
+    result = c1 + c2
+
+}
+
+
+sumar (5,10)
+
+console.log('el precio total es ' + result)
+alert('el precio total es ' + result)
+
